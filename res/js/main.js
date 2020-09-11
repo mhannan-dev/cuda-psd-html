3368591f5724cf209c913dea8ae73e8f0dc2808e
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2020-09-11 17:24:10 +0600

    done hamburger menu

--- res/js/main.js
+++ res/js/main.js
@@ -7,7 +7,28 @@ $(document).ready(function(){
          $("nav").removeClass("sticky");
       }
    });
+   //Active link
+   $("nav ul li a").click(function(){
+      $("nav ul li a").removeClass("active");
+      $(this).addClass("active");
+   })
+
+   $("nav a img.logo").click(function(){
+      $("nav ul li a").removeClass("active");
+      $("nav ul li::fitst-child a").addClass("active");
+      
+   })
+
+
    //Mixit up
    var mixer = mixitup('.gallery-container');
   });
+
+  function OpenNav(){
+     document.getElementById("myNav").style.width = "100%";
+  }
+
+  function CloseNav(){
+   document.getElementById("myNav").style.width = "0%";
+}
 
